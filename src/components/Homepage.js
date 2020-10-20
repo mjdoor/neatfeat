123e25a112b5a857f027576fe4eb98bca7859153
Logan Sovereign <logansovereign12@gmail.com>
2020-10-20 15:31:11 -0400

    more csv stuff

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,23 +1,69 @@
-import React from "react";
-import { Typography, Paper } from "@material-ui/core";
+import React, { useState } from "react";
+import { Typography, Paper, Button } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import { CSVReader, readRemoteFile } from "react-papaparse";
+import { Papa } from "papaparse";
 
 const Homepage = () => {
+  const [isUploaded, setIsUploaded] = useState(false);
+
+  const handleOnFileLoad = (data) => {
+    // readRemoteFile(
+    //   "https://raw.githubusercontent.com/loganSovereign/TestFiles/main/train.csv",
+    //   {
+    //     complete: (results) => {
+    //       var arrayOfData = results.data;
+    //     },
+    //   }
+    // );
+  };
+  const handleOnDrop = (data) => {
+    console.log("---------------------------");
+    console.log(data);
+    console.log("---------------------------");
+  };
+
+  const handleOnError = (err, file, inputElem, reason) => {
+    console.log(err);
+  };
+
   return (
-    <div style={{ padding: 10 }}>
-      <Paper
-        elevation={3}
-        style={{
-          width: "min-content",
-          margin: "0 auto",
-          padding: 20,
-          marginTop: 30,
-        }}
-        align="center"
-      >
-        <Typography variant="h3" color="primary" align="center" noWrap>
-          Neat Feat
-        </Typography>
-      </Paper>
+    <div style={{ padding: 10 }} align="center">
+      <AppBar>
+        <Toolbar>
+          <Typography color="secondary" align="left" noWrap variant="h4">
+            Neat Feat
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {!isUploaded && (
+        <CSVReader
+          onDrop={handleOnDrop}
+          onError={handleOnError}
+          noDrag
+          style={{
+            dropArea: {
+              width: "10px",
+              height: "10px",
+              borderColor: "#fff",
+              marginTop: "3.5em",
+              marginRight: "2em",
+              float: "right",
+            },
+          }}
+        >
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
+        </CSVReader>
+      )}
     </div>
   );
 };
