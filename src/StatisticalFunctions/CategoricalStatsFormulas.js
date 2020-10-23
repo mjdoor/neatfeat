1259bbbcda230cf5baa4ca67ddca4053c494c27e
sue1007 <37780021+sue1007@users.noreply.github.com>
2020-10-23 14:43:03 -0400

    updated curly bracket
    I missed the curly bracket when merging :(

--- src/StatisticalFunctions/CategoricalStatsFormulas.js
+++ src/StatisticalFunctions/CategoricalStatsFormulas.js
@@ -1,12 +1,4 @@
 export const filterOutNull = arr => {
-  return arr.filter(el => el !== null && el !== undefined);
-};
-
-export const valueCounts = arr => {
-  return arr.reduce((counts, val) => {
-    counts[val] = counts[val] === undefined ? 1 : ++counts[val];
-    return counts;
-  }, {});
   const removedIndices = [];
   const filteredArr = arr.filter((el, idx) => {
     const keep = el !== null && el !== undefined;
@@ -17,6 +9,13 @@ export const valueCounts = arr => {
   });
   return { filteredArr, removedIndices };
 };
+
+export const valueCounts = arr => {
+  const valueCounts = arr.reduce((counts, val) => {
+    counts[val] = counts[val] === undefined ? 1 : ++counts[val];
+    return counts;
+  }, {});
+
   // sorts the valueCounts object keys by value, descending
   return Object.fromEntries(
     Object.entries(valueCounts).sort((lhs, rhs) => rhs[1] - lhs[1])
