10f7896365d53c3a21933452cb01b1e85de8c1ab
sue koh <sue_koh@icloud.com>
2020-11-01 21:54:05 -0500

    count-dropdown

--- src/components/DataGridTable.js
+++ src/components/DataGridTable.js
@@ -2,12 +2,16 @@ import React, { useEffect } from "react";
 import DataGrid from "react-data-grid";
 import "react-data-grid/dist/react-data-grid.css";
 import { useSelector } from "react-redux";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { countOutliers } from "../StatisticalFunctions/NumericStatsFormulas";
 //import { DataGrid } from "@material-ui/data-grid";
 
 const DataGridTable = () => {
   const [rows, setRows] = React.useState([]);
   const [columns, setColumns] = React.useState([]);
-  const { rawData, columnNames } = useSelector(state => state);
+  const [count, setCount] = React.useState(0);
+  const { rawData, columnNames } = useSelector((state) => state);
 
   useEffect(() => {
     if (
@@ -16,10 +20,10 @@ const DataGridTable = () => {
       columnNames !== undefined &&
       columnNames.length > 0
     ) {
-      const columnOutputData = columnNames.map(colName => {
+      const columnOutputData = columnNames.map((colName) => {
         return {
           key: colName,
-          name: colName
+          name: colName,
         };
       });
       const rawDataWithIds = rawData[0].hasOwnProperty("id")
@@ -34,8 +38,59 @@ const DataGridTable = () => {
   }, [rawData, columnNames]);
 
   return (
-    <div style={{ padding: 10 }}>
-      {columns.length > 0 && <DataGrid columns={columns} rows={rows} />}
+    <div style={{ padding: 10, height: "400px" }}>
+      {columns.length > 0 && (
+        <div>
+          <div style={{ marginLeft: "80%" }}>
+            <DropdownButton
+              id="dropdown-basic-button"
+              variant="secondary"
+              title="Number of Rows"
+              size="sm"
+              style={{ marginBottom: "10px" }}
+            >
+              <Dropdown.Item
+                onSelect={() => {
+                  setCount(rows.length);
+                }}
+              >
+                All
+              </Dropdown.Item>
+              <Dropdown.Item
+                onSelect={() => {
+                  setCount(25);
+                }}
+              >
+                25
+              </Dropdown.Item>
+              <Dropdown.Item
+                onSelect={() => {
+                  setCount(50);
+                }}
+              >
+                50
+              </Dropdown.Item>
+              <Dropdown.Item
+                onSelect={() => {
+                  setCount(100);
+                }}
+              >
+                100
+              </Dropdown.Item>
+              <Dropdown.Item
+                onSelect={() => {
+                  setCount(150);
+                }}
+              >
+                150
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
+          <div style={{ marginTop: "10px" }}>
+            <DataGrid columns={columns} rows={rows} rowsCount={count} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
