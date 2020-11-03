a961c4fc390af02b6c19162499a824d5c141baf9
Mitchell Dooreleyers <mdoorele@gmail.com>
2020-11-03 15:38:44 -0500

    Remove unused code

--- src/components/DataGridTable.js
+++ src/components/DataGridTable.js
@@ -1,17 +1,11 @@
 import React, { useEffect } from "react";
-//import DataGrid from "react-data-grid";
-import "react-data-grid/dist/react-data-grid.css";
 import { useSelector } from "react-redux";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import { countOutliers } from "../StatisticalFunctions/NumericStatsFormulas";
 import { DataGrid } from "@material-ui/data-grid";
 
 const DataGridTable = () => {
   const [rows, setRows] = React.useState([]);
   const [columns, setColumns] = React.useState([]);
-  const [count, setCount] = React.useState(0);
-  const { rawData, columnNames } = useSelector((state) => state);
+  const { rawData, columnNames } = useSelector(state => state);
 
   useEffect(() => {
     if (
@@ -20,11 +14,11 @@ const DataGridTable = () => {
       columnNames !== undefined &&
       columnNames.length > 0
     ) {
-      const columnOutputData = columnNames.map((colName) => {
+      const columnOutputData = columnNames.map(colName => {
         return {
           field: colName,
           headerName: colName,
-          width: 150,
+          width: 150
         };
       });
       const rawDataWithIds = rawData[0].hasOwnProperty("id")
@@ -39,11 +33,16 @@ const DataGridTable = () => {
   }, [rawData, columnNames]);
 
   return (
-    <div style={{ height: "400px", marginBottom: "50px" }}>
+    <div style={{ height: "400px", marginBottom: "50px", padding: 10 }}>
       {columns.length > 0 && (
         <div>
           <div style={{ marginTop: "10px", height: "400px" }}>
-            <DataGrid columns={columns} rows={rows} />
+            <DataGrid
+              columns={columns}
+              rows={rows}
+              rowsPerPageOptions={[5, 10, 25, 50, 100]}
+              pageSize={5}
+            />
           </div>
         </div>
       )}
